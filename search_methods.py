9fe1fea5a1d8e560de43302ea917e1f1a28e53d5
Maciej Pawlikowski <pawlikowski.maciek@gmail.com>
2017-01-06 16:36:39 +0100

    added searching with lower bound

---
+++ search_methods.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+
+from graph_representation import Graph
+import time
+
+
+def search_with_lower_bound(n, m, problem, initial_solution, num_iter):
+    G = Graph(n, m, problem, initial_solution)
+    G.topological_sort()
+    cost_time = 0.
+    
+    for i in xrange(num_iter):
+        N = G.generate_neighborhood()
+        
+        t0 = time.time()
+        best_move = min(N, key=G.lower_bound)
+        cost_time += time.time()-t0
+        
+        G.make_a_move(best_move)
+        
+        t0 = time.time()
+        G.topological_sort()
+        cost_time += time.time()-t0
+        
+        print best_move
+        
+    return G.solution, cost_time
